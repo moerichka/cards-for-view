383ec2696cec1a9e4724cc85dc857e45cc1de1a4
MichailI <mishika1221@mail.ru>
2023-02-20 23:49:11 +0600

    Инициализирован проект и добавлена верстка страницы

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable react/jsx-props-no-spreading */
+import "styles/main.scss";
+import type { AppProps } from "next/app";
+
+import HeightSetter from "components/HeightSetter";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <HeightSetter />
+    </>
+  );
 }
