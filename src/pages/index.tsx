77629f4b4b3552d67f209fadf25ee812e5e289bf
MichailI <mishika1221@mail.ru>
2023-02-26 13:50:29 +0600

    Добавлена заглушка Loading, перед загрузкой изображений
    Co-authored-by: Alexander Tambulov <tabowillow@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,49 @@
+import { useState, useCallback, useMemo, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Loader from "react-loaders";
 
 import s from "styles/home.module.scss";
 
 import cubeImage from "../../public/images/cube.png";
 import backgroundImage from "../../public/images/skyBanner.jpg";
 
+const EXPECTED_LOADED_IMAGES_COUNT = 2;
+
 export default function Home() {
+  const [loadedImagesCount, setLoadedImagesCount] = useState<number>(0);
+  const [isHiding, setIsHiding] = useState<boolean>(false);
+
+  const incrementLoadedImagesCount = useCallback(
+    () => setLoadedImagesCount((i) => i + 1),
+    [],
+  );
+
+  const isAllImagesLoaded = loadedImagesCount === EXPECTED_LOADED_IMAGES_COUNT;
+
+  useEffect(() => {
+    if (isAllImagesLoaded) {
+      setIsHiding(true);
+      setTimeout(() => {
+        setIsHiding(false);
+      }, 300);
+    }
+  }, [isAllImagesLoaded]);
+
+  const loadingClassName = useMemo(
+    () =>
+      `${s.loadingStub} ${isAllImagesLoaded ? s.hidden : ""} ${
+        isHiding ? s.hiding : ""
+      }`,
+    [isAllImagesLoaded, isHiding],
+  );
+
   return (
     <>
       <Head>
         <title>Trace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#499EDD" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={s.bannerPage}>
@@ -30,8 +62,8 @@ export default function Home() {
               src={cubeImage}
               alt=""
               style={{ objectFit: "contain" }}
-              placeholder="blur"
               quality={100}
+              onLoad={incrementLoadedImagesCount}
             />
           </div>
           <div className={s.button}>
@@ -73,8 +105,13 @@ export default function Home() {
             quality={100}
             style={{ objectFit: "cover" }}
             alt=""
-            placeholder="blur"
+            onLoad={incrementLoadedImagesCount}
           />
+        </div>
+        <div className={loadingClassName}>
+          <div className={s.loaderWrapper}>
+            <Loader type="line-scale-pulse-out-rapid" active />
+          </div>
         </div>
       </div>
     </>
