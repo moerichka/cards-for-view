89d4f29a9e410b73a660b5d4c96ecba76e88af8c
MichailI <mishika1221@mail.ru>
2023-03-09 13:07:40 +0600

    Добавлен адаптив для страницы CardPage

--- src/pages/cards/[cardId]/index.tsx
+++ src/pages/cards/[cardId]/index.tsx
@@ -47,64 +47,87 @@ function CardPage() {
 
   return (
     <div className={s.cardPage}>
-      <div className={s.container}>
-        <div className={s.grid}>
-          <div className={s.header}>
-            <div className={s.logoWrapper}>
+      <div className={s.grid}>
+        <div className={s.header}>
+          <div className={s.logoWrapper}>
+            <Image
+              fill
+              src="/images/logoBlue.png"
+              alt="Logo"
+              style={{ objectFit: "contain" }}
+            />
+            <Link href="/cards" className="linkFill" />
+          </div>
+          <Link href="/cards" className={s.closeButton}>
+            <span className={s.line} />
+            <span className={s.line} />
+          </Link>
+        </div>
+        <div className={`${s.titlePart} ${s.desktop}`}>
+          <div className={s.chapterNumber}>
+            Chapter {thisChapter.chapterNumber}
+          </div>
+          <div className={s.title}>{thisChapter?.title}</div>
+        </div>
+        <div className={s.chapter}>
+          <div className={s.chapterBanner}>
+            <div className={s.imageWrapper}>
               <Image
+                src={thisChapter.image}
+                style={{ objectFit: "cover" }}
                 fill
-                src="/images/logoBlue.png"
-                alt="Logo"
-                style={{ objectFit: "contain" }}
+                alt=""
               />
-              <Link href="/cards" className="linkFill" />
+            </div>
+            <div className={s.dateWrapper}>
+              <div className={s.date}>{thisChapter.date}</div>
             </div>
           </div>
-          <div className={s.titlePart}>
-            <div className={s.chapterNumber}>
-              Chapter {thisChapter.chapterNumber}
+          <div className={s.buttons}>
+            <ButtonRoute
+              href="/cards"
+              className={`${s.button} ${s.noMobile}`}
+              variant="blue"
+            >
+              Back
+            </ButtonRoute>
+            <ButtonLink href="/" className={s.button}>
+              Join
+            </ButtonLink>
+          </div>
+          <div className={`${s.titlePart} ${s.tablet}`}>
+            <div className={s.topPart}>
+              <div className={s.chapterNumber}>
+                Chapter {thisChapter.chapterNumber}
+              </div>
+              <div className={s.date}>{thisChapter.date}</div>
             </div>
             <div className={s.title}>{thisChapter?.title}</div>
           </div>
-          <div className={s.chapter}>
-            <div className={s.chapterBanner}>
-              <div className={s.imageWrapper}>
-                <Image
-                  src={thisChapter.image}
-                  style={{ objectFit: "cover" }}
-                  fill
-                  alt=""
+          <div className={s.chapterText}>{thisChapter.text}</div>
+        </div>
+        <div className={s.tabs}>
+          {chapters.map((chapter) => (
+            <div className={s.tabWrapper} key={chapter.id}>
+              <ChapterTab chapter={chapter} />
+              {chapter.status === "available" && (
+                <Link
+                  href={`/cards/${chapter.id}`}
+                  className="linkFill"
+                  style={{ zIndex: 5 }}
                 />
-              </div>
-              <div className={s.dateWrapper}>
-                <div className={s.date}>{thisChapter.date}</div>
-              </div>
+              )}
             </div>
-            <div className={s.chapterText}>{thisChapter.text}</div>
-          </div>
-          <div className={s.tabs}>
-            {chapters.map((chapter) => (
-              <div className={s.tabWrapper} key={chapter.id}>
-                <ChapterTab chapter={chapter} />
-                {chapter.status === "available" && (
-                  <Link
-                    href={`/cards/${chapter.id}`}
-                    className="linkFill"
-                    style={{ zIndex: 5 }}
-                  />
-                )}
-              </div>
-            ))}
-          </div>
-          <div className={s.footer}>
-            <div className={s.buttons}>
-              <ButtonRoute href="/cards" className={s.button} variant="blue">
-                Back
-              </ButtonRoute>
-              <ButtonLink href="/" className={s.button}>
-                Join
-              </ButtonLink>
-            </div>
+          ))}
+        </div>
+        <div className={s.footer}>
+          <div className={s.buttons}>
+            <ButtonRoute href="/cards" className={s.button} variant="blue">
+              Back
+            </ButtonRoute>
+            <ButtonLink href="/" className={s.button}>
+              Join
+            </ButtonLink>
           </div>
         </div>
       </div>
